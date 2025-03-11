1af12b1d120259e6af3fc0b3b7a438be2e972fef
rajivkenzo23 <rajivkenzo23@gmail.com>
2025-03-11 15:19:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     autoLikeStatus: process.env.AUTO_LIKE_STATUS === 'true',
-    downloadMediaStatus: process.env.DOWNLOAD_MEDIA_STATUS === 'true',
+    downloadMediaStatus: process.env.DOWNLOAD_MEDIA_STATUS === 'false',
     sensorNumber: process.env.SENSOR_NUMBER === 'true',
     sendWelcomeMessage: process.env.SEND_WELCOME_MESSAGE === 'true',
 
@@ -8,19 +8,19 @@ module.exports = {
     whiteList: process.env.WHITE_LIST ? process.env.WHITE_LIST.split(',') : [],
 
     fenixaboutype: process.env.FENIX_ABOUT_TYPE || 'Programming',
-    ownerNumber: process.env.OWNER_NUMBER || '94773010580',
+    ownerNumber: process.env.OWNER_NUMBER || '94760457315',
     userTimezone: process.env.USER_TIMEZONE || 'Asia/Colombo',
     FenixName: process.env.FENIX_NAME || 'Unknown Friend',
 
     // Group contact push
-    fenixwel1: process.env.FENIX_WEL_1 || 'Your Number Has Been Auto Saved🫠',
-    fenixwel2: process.env.FENIX_WEL_2 || 'Save And Alert Me If Save 🌹',
+    fenixwel1: process.env.FENIX_WEL_1 || 'ඔයාගෙ නම්බර් එක සේව් වෙලා තියෙන්නෙ...🫠',
+    fenixwel2: process.env.FENIX_WEL_2 || 'සේව්✅ නම් ` Save`  or ` sv ` කියලා මැසේජ් එකක් දාන්න.නැත්තන් Contact අයින් වෙයි🌹',
     fenixwel3: process.env.FENIX_WEL_3 || 'Save and Alert me✅',
 
     // Bot details
     botDetails: {
-        botName: process.env.BOT_NAME || 'FENX ID OWNER',
-        botAge: process.env.BOT_AGE || '26',
+        botName: process.env.BOT_NAME || 'VIXEN ID OWNER',
+        botAge: process.env.BOT_AGE || '22',
         botLocation: process.env.BOT_LOCATION || 'SRILANKA',
         botEmail: process.env.BOT_EMAIL || 'privateloginemails@gmail.com',
     },
